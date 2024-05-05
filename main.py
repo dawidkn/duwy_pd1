60c902274087cd55228909db77dd517747d281c9
dawidkn <dawid.knas15@gmail.com>
2024-05-05 20:26:13 +0200

    pierwszy commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('hello')
+import numpy as np
+
+def oblicz_transformacje(punkt_referencyjny, kat):
+    # Obliczenie macierzy obrotu dla danego kąta
+    macierz_obrotu = np.array([[np.cos(kat), -np.sin(kat)],
+                               [np.sin(kat), np.cos(kat)]])
+    # Przesunięcie do punktu referencyjnego
+    przesuniecie = np.array(punkt_referencyjny)
+    return macierz_obrotu, przesuniecie
+
+# Przykład użycia funkcji
+punkt_referencyjny = (0, 0)
+kat = np.pi/4  # Przykładowy kąt (45 stopni)
+transformacja = oblicz_transformacje(punkt_referencyjny, kat)
+macierz_obrotu, przesuniecie = transformacja
+print("Macierz obrotu:")
+print(macierz_obrotu)
+print("Przesunięcie:")
+print(przesuniecie)
